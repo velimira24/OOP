ae845085ad56b1b40f8e6f41a4468f84649d1ee9
Velimira <revekka-best@mail.ru>
2023-05-21 14:44:10 +0300

    Homework

---
+++ Sem6/4/Task4/src/Shapes/Rectangle.java
@@ -0,0 +1,23 @@
+package Shapes;
+
+public class Rectangle {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int area() {
+        return this.width * this.height;
+    }
+}
---
+++ Sem6/4/Task4/src/Shapes/Square.java
@@ -0,0 +1,18 @@
+package Shapes;
+
+public class Square extends Rectangle {
+    public Square(int sideLength) {
+        super(sideLength, sideLength);
+    }
+
+    @Override
+    public void setWidth(int sideLength) {
+
+    }
+
+    @Override
+    public void setHeight(int sideLength) {
+
+    }
+
+}
